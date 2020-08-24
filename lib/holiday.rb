ec22e9c22383ed010fe3ef8319e78b92a56540d9
Dario Carlino <baires82@gmail.com>
2020-08-24 05:09:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
       sttr = season.to_s
       sttr.gsub("_", " ")
-      puts "#{sttr}:"
+      puts "#{sttr.capitalize}:"
       holiday.each do |hol, stuff|
         b = stuff.join(", ")
         c = hol.capitalize

4d4886e9abb64a74cebec72c2d4e0766f5f6e861
Dario Carlino <baires82@gmail.com>
2020-08-24 05:04:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,11 +79,19 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season}"
+
+    puts "#{season}:"
+    holiday.each do |hol, stuff|
+      b = stuff.join(", ")
+      c = hol.capitalize
+      puts "  #{c}: #{b}"
+    end
   end
+
 end
 
 
+def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 

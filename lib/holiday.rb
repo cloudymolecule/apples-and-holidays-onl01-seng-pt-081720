a598dbd46be27171b5e8d483cb03a391ade3dc38
Dario Carlino <baires82@gmail.com>
2020-08-24 05:23:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
+    puts "#{(holiday).to_s.capitalize}"
   end
 end
 
